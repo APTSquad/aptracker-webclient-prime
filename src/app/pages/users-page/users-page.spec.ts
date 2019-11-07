ea5054a6fd3c72182953a1e1c89bec92dc24da36
D5T <cyanogen.gg@gmail.com>
2019-11-07 11:19:30 +0400

    Fix tests

--- src/app/pages/users-page/users-page.spec.ts
+++ src/app/pages/users-page/users-page.spec.ts
@@ -1,36 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { UsersListPageComponent } from './users-page';
 
-import { ClientListPageComponent } from './users-page';
-import {
-  MatFormFieldModule,
-  MatTableModule,
-  MatPaginatorModule,
-  MatInputModule,
-  MatIconModule
-} from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('ClientListPageComponent', () => {
-  let component: ClientListPageComponent;
-  let fixture: ComponentFixture<ClientListPageComponent>;
+describe('UsersListPageComponent', () => {
+  let component: UsersListPageComponent;
+  let fixture: ComponentFixture<UsersListPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        MatFormFieldModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatInputModule,
-        MatIconModule,
-        BrowserAnimationsModule
-      ],
-      declarations: [ClientListPageComponent]
+      declarations: [UsersListPageComponent]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ClientListPageComponent);
+    fixture = TestBed.createComponent(UsersListPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
